db6b3d174d0cff3e793fca7aa4799e1ec61d2658
yangguoxiao <yangygx@qq.com>
2017-07-04 18:37:18 +0800

    创建

---
+++ src/com/atguigu/test/Test.java
@@ -0,0 +1,13 @@
+package com.atguigu.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int i =1;
+		int j =++i;
+		//前++先赋自增在运算
+		//后++先赋值后运算
+		System.out.println(j);
+		System.out.println(i);
+	}
+}
